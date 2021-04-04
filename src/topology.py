981a41ee302016d41672a42d1bd22c92c1eacd46
ewa.golab1998 <ewa.golab1998@gmail.com>
2021-04-04 12:47:29 +0200

    add more fixes

--- src/topology.py
+++ src/topology.py
@@ -28,13 +28,14 @@ class TreeTopology(Topo):
             for host in range(hosts):
                 host_name = 'h{}'.format(host + index + 1)
                 self.addHost(host_name, ip = '10.0.0.{}'.format(host + index + 1))
-                self.addLink(host_name, switch_name, bw=10, delay='1ms', loss=5)
+                self.addLink(host_name, switch_name, bw = 10, delay = '1ms', loss = 5)
 
             index = index + hosts
 
 
-def stream(net, hosts):
-    server_command = 'cvlc ./samples/audio.mp3 --sout "#standard{access=http,mux=ogg,dst=0.0.0.0:8080}" --run-time 30 vlc://quit &'
+def stream(net, hosts, path, duration):
+    delay = 5
+    server_command = 'cvlc ./samples/{path} --sout "#standard{{access=http,mux=ogg,dst=0.0.0.0:8080}}" --run-time {duration} vlc://quit &'.format(path = path, duration = str(duration + delay))
     client_command = 'cvlc http://10.0.0.254:8080 &'
     run_as_root = "sed -i 's/geteuid/getppid/' /usr/bin/vlc"
 
@@ -44,10 +45,10 @@ def stream(net, hosts):
     h0.cmd(server_command)
 
     info('*** Starting VLC clients...\n')
-    time.sleep(5)
+    time.sleep(delay)
 
     for h in range(hosts):
-        host = net.get('h{}'.format(h+1))
+        host = net.get('h{}'.format(h + 1))
         host.cmd(client_command)
 
 
@@ -55,26 +56,36 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--switches', help = 'Number of Switches')
     parser.add_argument('--hosts', help = 'Number of Hosts connected to each switch')
+    parser.add_argument('--file', help = 'Audio file that will be streamed')
+    parser.add_argument('--duration', help = 'Streaming duration')
     args = parser.parse_args()
 
-    s = 2
-    h = 1
+    switches = 2
+    hosts = 1
+    file = 'audio.mp3'
+    duration = 30
 
     if args.switches:
-        s = int(args.switches)
+        switches = int(args.switches)
 
     if args.hosts:
-        h = int(args.hosts)
+        hosts = int(args.hosts)
+
+    if args.file:
+        file = args.file
+
+    if args.duration:
+        duration = int(args.duration)
 
 
     setLogLevel('info')
-    topo = TreeTopology(switches = s, hosts = h)
-    net = Mininet(topo=topo, link=TCLink, host=CPULimitedHost)
-    c = net.addController('c', controller=Controller, ip='127.0.0.1', port=6633)
+    topo = TreeTopology(switches = switches, hosts = hosts)
+    net = Mininet(topo = topo, link = TCLink, host = CPULimitedHost)
+    c = net.addController('c', controller = Controller, ip = '127.0.0.1', port = 6633)
 
     net.addNAT().configDefault()
     net.start()
-    stream(net, hosts=h)
+    stream(net, hosts = hosts, path = file, duration = duration)
     CLI(net)
 
     net.stop()
