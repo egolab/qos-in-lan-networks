ef384dd90e118c409f438e960629234c88f96736
egolab <57237362+egolab@users.noreply.github.com>
2021-04-04 14:02:04 +0200

    Add VLC streaming function (#3)
    * Add streaming function
    
    * Fix streaming function
    
    * Update topology.py
    
    * Add some fixes
    
    * Add Makefile (#4)
    
    * update gitignore
    
    * add makefile
    
    * reorganized dir structure
    
    * update readme
    
    * reorder structure once again
    
    * update readme
    
    * one more readme update
    
    * info about installing make
    
    * add clean & fix path
    
    * review fix
    
    * add some fixes
    
    * add more fixes
    
    * update readme
    
    * add fixes
    
    * fixes
    
    Co-authored-by: Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>

--- src/topology.py
+++ src/topology.py
@@ -8,10 +8,12 @@ from mininet.node import Controller
 from mininet.node import CPULimitedHost, Host
 from mininet.link import TCLink
 import argparse
+import os, sys, time
+import os.path
 
 
 class TreeTopology(Topo):
-    def __init__(self, switches = 2, hosts = 5):
+    def __init__(self, switches, hosts):
         Topo.__init__(self)
 
         self.addSwitch('s0') # main switch
@@ -26,35 +28,71 @@ class TreeTopology(Topo):
 
             for host in range(hosts):
                 host_name = 'h{}'.format(host + index + 1)
-                self.addHost(host_name, ip = "10.0.0.{}".format(host + index + 1))
-                self.addLink(host_name, switch_name, bw=10) # (host, switch, bw=10, delay='5ms', loss=10)
+                self.addHost(host_name, ip = '10.0.0.{}'.format(host + index + 1))
+                self.addLink(host_name, switch_name, bw = 10, delay = '1ms', loss = 5)
 
             index = index + hosts
 
 
+def stream(net, hosts, path, duration):
+    delay = 5
+    server_command = 'cvlc {path} --sout "#standard{{access=http,mux=ogg,dst=0.0.0.0:8080}}" --run-time {duration} vlc://quit &'.format(path = path, duration = str(duration + delay))
+    client_command = 'cvlc http://10.0.0.254:8080 &'
+    run_as_root = "sed -i 's/geteuid/getppid/' /usr/bin/vlc"
+
+    info('*** Starting VLC server...\n')
+    h0 = net.get('h0')
+    h0.cmd(run_as_root)
+    h0.cmd(server_command)
+
+    info('*** Starting VLC clients...\n')
+    time.sleep(delay)
+
+    for h in range(hosts):
+        host = net.get('h{}'.format(h + 1))
+        host.cmd(client_command)
+
+    info('*** VLC server and clients started\n')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--switches", help = "Number of Switches")
-    parser.add_argument("--hosts", help = "Number of Hosts connected to each switch")
+    parser.add_argument('--switches', help = 'Number of Switches')
+    parser.add_argument('--hosts', help = 'Number of Hosts connected to each switch')
+    parser.add_argument('--file', help = 'Audio file that will be streamed')
+    parser.add_argument('--duration', help = 'Streaming duration')
     args = parser.parse_args()
 
-    s = 2
-    h = 5
+    switches = 2
+    hosts = 1
+    file = './samples/audio.mp3'
+    duration = 30
 
     if args.switches:
-        s = int(args.switches)
+        switches = int(args.switches)
 
     if args.hosts:
-        h = int(args.hosts)
+        hosts = int(args.hosts)
+
+    if args.file:
+        file = args.file
+        if not os.path.isfile(file):
+            print ("File doesn't exist")
+            exit()
+
+    if args.duration:
+        duration = int(args.duration)
 
     setLogLevel('info')
-    topo = TreeTopology(switches = s, hosts = h)
-    net = Mininet(topo=topo, link=TCLink, host=CPULimitedHost)
-    c = net.addController('c', controller=Controller, ip='127.0.0.1', port=6633)
+    topo = TreeTopology(switches = switches, hosts = hosts)
+    net = Mininet(topo = topo, link = TCLink, host = CPULimitedHost)
+    c = net.addController('c', controller = Controller, ip = '127.0.0.1', port = 6633)
 
     net.addNAT().configDefault()
     net.start()
-    net.pingAll()
+    stream(net, hosts = hosts, path = file, duration = duration)
     CLI(net)
 
     net.stop()
+    os.system('sudo mn -c')
+    info('*** You\'ve successfully exited mininet\n')
